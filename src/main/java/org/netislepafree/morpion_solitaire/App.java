6e360b1c34b941aaa93e18dca28981035630bccb
MOQRICHAyoub <ayoubmoqrich@gmail.com>
2023-10-23 09:18:21 +0200

    FX

--- src/main/java/org/netislepafree/morpion_solitaire/App.java
+++ src/main/java/org/netislepafree/morpion_solitaire/App.java
@@ -1,14 +1,15 @@
 package org.netislepafree.morpion_solitaire;
 
+import java.io.IOException;
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import java.io.IOException;
 
 public class App extends Application {
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("../../../../resources/game-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("game-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(),20,100);
         stage.setTitle("Morpion solitaire");
         stage.setScene(scene);
